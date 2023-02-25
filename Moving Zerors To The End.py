2e3756ed170304a792083e0ce78f068c1bd4959e
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2023-02-25 19:06:48 +0530

    Update Moving Zerors To The End.py

--- Moving Zerors To The End.py
+++ Moving Zerors To The End.py
@@ -1,11 +1,1 @@
-def move_zeros(array):
-    a=[]
-    s=0
-    for i in array:
-        if i==0:
-            s=s+1
-        else:
-            a.append(i)
-    for j in range(s):
-        a.append(0)
-    return a
+
